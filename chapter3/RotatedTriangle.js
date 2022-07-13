1dcb402203f2b971546bbeb01dae597d85740838
qin.zehua <775935087@qq.com>
2022-07-13 23:44:36 +0800

    旋转

---
+++ chapter3/RotatedTriangle.js
@@ -0,0 +1,43 @@
+const vshaderCode = document.getElementById("vshader").innerHTML;
+const fshaderCode = document.getElementById("fshader").innerHTML;
+
+function main() {
+  const canvas = document.getElementById("canvas");
+  const gl = getWebGLContext(canvas);
+
+  if (!initShaders(gl, vshaderCode, fshaderCode)) {
+    console.log("error");
+  }
+
+  const n = initVertexBuffers(gl);
+  initRotation(gl);
+  gl.clearColor(0.5, 0.5, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+
+  function initVertexBuffers(gl) {
+    const a_Position = gl.getAttribLocation(gl.program, "a_Position");
+    const vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+    const vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+    const n = 4;
+    return n;
+  }
+
+  function initRotation(gl) {
+    var ANGLE = 90;
+    var radian = (Math.PI * ANGLE) / 180.0;
+    console.log(radian);
+    const cosB = Math.cos(radian);
+    const sinB = Math.sin(radian);
+    const u_CosB = gl.getUniformLocation(gl.program, "u_CosB");
+    const u_SinB = gl.getUniformLocation(gl.program, "u_SinB");
+
+    gl.uniform1f(u_CosB, cosB);
+    gl.uniform1f(u_SinB, sinB);
+  }
+}
