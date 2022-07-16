aa1685c39f73c70b7ecafd6168af41e933c4ecbd
qin.zehua <775935087@qq.com>
2022-07-16 08:24:38 +0800

    使用第三方矩阵库

--- chapter4/RotateTriangle_Matrix4.js
+++ chapter4/RotateTriangle_Matrix4.js
@@ -4,62 +4,45 @@ const fshaderCode = document.getElementById("fshader").innerHTML;
 function main() {
   const canvas = document.getElementById("canvas");
   const gl = getWebGLContext(canvas);
+  let da = 10;
 
   if (!initShaders(gl, vshaderCode, fshaderCode)) {
     console.log("error");
   }
+  gl.clearColor(0.5, 0.5, 0.0, 1.0);
 
   const n = initVertexBuffers(gl);
   initRotation(gl);
   draw(gl, n);
   function initVertexBuffers(gl) {
     const a_Position = gl.getAttribLocation(gl.program, "a_Position");
-    const vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+    const vertices = new Float32Array([0.0, 0.3, -0.3, -0.3, 0.3, -0.3]);
     const vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(a_Position);
-    const n = 4;
+    const n = 3;
     return n;
   }
 
-  function initRotation(gl, ANGLE = 10, dx = 0.1) {
-    var radian = (Math.PI * ANGLE) / 180.0;
-    const cosB = Math.cos(radian);
-    const sinB = Math.sin(radian);
+  function initRotation(gl, ANGLE = 90) {
+    var xformMatrix = new Matrix4();
+    xformMatrix.setRotate(ANGLE, 0, 0, 1);
+    xformMatrix.translate(0.35, 0, 0);
 
-    //prettier-ignore
-    /* [
-        cosB,-sinB,0,0,
-        sinB,cosB, 0,0,
-        0,   0,    1,0,
-        0,   0,    0,0
-    ]*/
-    const xformMatri = new Float32Array([
-        cosB,sinB,0,0,
-        -sinB,cosB,0,0,
-        0,   0,    1,0,
-        dx,   0,    0,1
-    ])
-    console.log(xformMatri);
     const u_xformMatrix = gl.getUniformLocation(gl.program, "u_xformMatrix");
-    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatri);
+    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix.elements);
   }
 
   function draw(gl, n) {
-    gl.clearColor(0.5, 0.5, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLES, 0, n);
-  }
-
-  let da = 10;
-  let dx = 0.05;
-  canvas.addEventListener("click", () => {
     da += 10;
-    dx += 0.05;
-    initRotation(gl, da, dx);
-    draw(gl, n);
-  });
+    requestAnimationFrame(() => {
+      initRotation(gl, da);
+      draw(gl, n);
+    });
+  }
 }
--- chapter4/RotatingTriangle.js
+++ chapter4/RotatingTriangle.js
@@ -14,38 +14,25 @@ function main() {
   draw(gl, n);
   function initVertexBuffers(gl) {
     const a_Position = gl.getAttribLocation(gl.program, "a_Position");
-    const vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+    const vertices = new Float32Array([0.0, 0.3, -0.3, -0.3, 0.3, -0.3]);
     const vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(a_Position);
-    const n = 4;
+    const n = 3;
     return n;
   }
 
-  function initRotation(gl, ANGLE = 10, dx = 0.1) {
-    var radian = (Math.PI * ANGLE) / 180.0;
-    const cosB = Math.cos(radian);
-    const sinB = Math.sin(radian);
-
-    //prettier-ignore
-    /* [
-        cosB,-sinB,0,0,
-        sinB,cosB, 0,0,
-        0,   0,    1,0,
-        0,   0,    0,0
-    ]*/
-    const xformMatri = new Float32Array([
-        cosB,sinB,0,0,
-        -sinB,cosB,0,0,
-        0,   0,    1,0,
-        dx,   0,    0,1
-    ])
-    console.log(xformMatri);
+  function initRotation(gl, ANGLE = 90, dx = 0.5) {
+    var xformMatrix = new Matrix4();
+    xformMatrix.setRotate(ANGLE, 0, 0, 1);
+    xformMatrix.translate(dx, 0, 0);
+    // xformMatrix.setTranslate(dx, 0, 0);
+    // xformMatrix.rotate(ANGLE, 0, 0, 1);
     const u_xformMatrix = gl.getUniformLocation(gl.program, "u_xformMatrix");
-    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatri);
+    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix.elements);
   }
 
   function draw(gl, n) {
