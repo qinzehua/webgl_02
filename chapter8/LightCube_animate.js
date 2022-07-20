bc83faf4e22ba0a12a836d33a2b1c1e6a9e80398
qin.zehua <775935087@qq.com>
2022-07-20 23:06:25 +0800

    旋转立方体以及法线

---
+++ chapter8/LightCube_animate.js
@@ -0,0 +1,157 @@
+var VSHADER_SOURCE = document.getElementById("vshader").innerHTML;
+var FSHADER_SOURCE = document.getElementById("fshader").innerHTML;
+
+function main() {
+  var canvas = document.getElementById("webgl");
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log("Failed to get the rendering context for WebGL");
+    return;
+  }
+
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log("Failed to intialize shaders.");
+    return;
+  }
+
+  var n = initVertexBuffers(gl);
+
+  // 设置清除的背景色
+  gl.clearColor(0.0, 0.0, 0.0, 1);
+  //深度测试
+  gl.enable(gl.DEPTH_TEST);
+  //获取转换矩阵变量
+  const u_MvpMatrix = gl.getUniformLocation(gl.program, "u_MvpMatrix");
+  const u_NormalMatrix = gl.getUniformLocation(gl.program, "u_NormalMatrix");
+  const u_LightDirection = gl.getUniformLocation(
+    gl.program,
+    "u_LightDirection"
+  );
+
+  //创建矩阵
+  const verticx4 = new Matrix4();
+  //设置投影方式
+  verticx4.setPerspective(30, 1, 1, 100);
+  // 设置视点
+  verticx4.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
+
+  //光照方向
+  var lightDirection = new Vector3([0.5, 3.0, 4.0]);
+  lightDirection.normalize();
+  gl.uniform3fv(u_LightDirection, lightDirection.elements);
+
+  let angle = 0;
+  const rotateMatrix = new Matrix4();
+  var mvpMatrix = new Matrix4();
+  const normalMatrix = new Matrix4();
+  function tick() {
+    angle = animate(angle);
+    rotateMatrix.setRotate(angle, 0, 1, 0);
+    mvpMatrix.set(verticx4).multiply(rotateMatrix);
+    //往shader写入矩阵
+    gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+
+    normalMatrix.setInverseOf(rotateMatrix);
+    normalMatrix.transpose();
+    gl.uniformMatrix4fv(u_NormalMatrix, false, normalMatrix.elements);
+
+    //清理颜色缓冲区和深度缓冲区
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    // 绘制立方体
+    gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+    requestAnimationFrame(tick);
+  }
+  tick();
+}
+
+function initVertexBuffers(gl) {
+  // Create a cube
+  //    v6----- v5
+  //   /|      /|
+  //  v1------v0|
+  //  | |     | |
+  //  | |v7---|-|v4
+  //  |/      |/
+  //  v2------v3
+
+  // prettier-ignore
+  var verticesPositions = new Float32Array([
+    1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+    1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+    1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+   -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+   -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+    1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
+    ]);
+  const a_Position = gl.getAttribLocation(gl.program, "a_Position");
+  const positon_buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positon_buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesPositions, gl.STATIC_DRAW);
+
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Position);
+  // prettier-ignore
+  var verticesColors = new Float32Array([
+    0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
+    0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
+    1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
+    1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
+    1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
+    0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0   // v4-v7-v6-v5 back
+   ]);
+  const a_Color = gl.getAttribLocation(gl.program, "a_Color");
+  const color_buffer = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Color);
+
+  // prettier-ignore
+  var verticesNormal = new Float32Array([
+    0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
+    1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
+    0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,  // v0-v5-v6-v1 up
+   -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  // v1-v6-v7-v2 left
+    0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,  // v7-v4-v3-v2 down
+    0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0   // v4-v7-v6-v5 back
+     ]);
+  const a_Normal = gl.getAttribLocation(gl.program, "a_Normal");
+  const normal_buffer = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, normal_buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesNormal, gl.STATIC_DRAW);
+  gl.vertexAttribPointer(a_Normal, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Normal);
+
+  // prettier-ignore
+  // Indices of the vertices
+  var indices = new Uint8Array([
+    0, 1, 2,   0, 2, 3,    // front
+    4, 5, 6,   4, 6, 7,    // right
+    8, 9,10,   8,10,11,    // up
+   12,13,14,  12,14,15,    // left
+   16,17,18,  16,18,19,    // down
+   20,21,22,  20,22,23     // back
+   ]);
+
+  const index_buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  return indices.length;
+}
+
+// Rotation angle (degrees/second)
+var ANGLE_STEP = 30.0;
+// Last time that this function was called
+var g_last = Date.now();
+function animate(angle) {
+  // Calculate the elapsed time
+  var now = Date.now();
+  var elapsed = now - g_last;
+  g_last = now;
+  // Update the current rotation angle (adjusted by the elapsed time)
+  var newAngle = angle + (ANGLE_STEP * elapsed) / 1000.0;
+  return (newAngle %= 360);
+}
