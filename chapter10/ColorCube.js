2c4fa3a097d5142aabb141885e017ee6243c5dc2
qin.zehua <775935087@qq.com>
2022-07-24 13:37:52 +0800

    3维贴图

---
+++ chapter10/ColorCube.js
@@ -0,0 +1,143 @@
+var VSHADER_SOURCE = document.getElementById("vshader").innerHTML;
+var FSHADER_SOURCE = document.getElementById("fshader").innerHTML;
+
+var canvas = document.getElementById("webgl");
+
+function main() {
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log("Failed to get the rendering context for WebGL");
+    return;
+  }
+
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log("Failed to intialize shaders.");
+    return;
+  }
+
+  var n = initVertexBuffers(gl);
+
+  if (!initTextures(gl)) {
+    console.log("Failed to intialize the texture.");
+    return;
+  }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1);
+  gl.enable(gl.DEPTH_TEST);
+  const u_MvpMatrix = gl.getUniformLocation(gl.program, "u_MvpMatrix");
+
+  var tick = function () {
+    draw(gl, n, u_MvpMatrix);
+    requestAnimationFrame(tick, canvas);
+  };
+  tick();
+}
+
+const verticx4 = new Matrix4();
+verticx4.setPerspective(30.0, canvas.width / canvas.height, 1.0, 100.0);
+verticx4.lookAt(3.0, 3.0, 7.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+var g_MvpMatrix = new Matrix4();
+
+function draw(gl, n, u_MvpMatrix) {
+  g_MvpMatrix.set(verticx4);
+  gl.uniformMatrix4fv(u_MvpMatrix, false, g_MvpMatrix.elements);
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+}
+
+function initVertexBuffers(gl) {
+  // Create a cube
+  //    v6----- v5
+  //   /|      /|
+  //  v1------v0|
+  //  | |     | |
+  //  | |v7---|-|v4
+  //  |/      |/
+  //  v2------v3
+
+  // prettier-ignore
+  var verticesPositions = new Float32Array([
+    1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,    // v0-v1-v2-v3 front
+    1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,    // v0-v3-v4-v5 right
+    1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,    // v0-v5-v6-v1 up
+   -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,    // v1-v6-v7-v2 left
+   -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,    // v7-v4-v3-v2 down
+    1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0     // v4-v7-v6-v5 back
+    ]);
+  // prettier-ignore
+  var texCoords = new Float32Array([   // Texture coordinates
+  1.0, 1.0,   0.0, 1.0,   0.0, 0.0,   1.0, 0.0,    // v0-v1-v2-v3 front
+  0.0, 1.0,   0.0, 0.0,   1.0, 0.0,   1.0, 1.0,    // v0-v3-v4-v5 right
+  1.0, 0.0,   1.0, 1.0,   0.0, 1.0,   0.0, 0.0,    // v0-v5-v6-v1 up
+  1.0, 1.0,   0.0, 1.0,   0.0, 0.0,   1.0, 0.0,    // v1-v6-v7-v2 left
+  0.0, 0.0,   1.0, 0.0,   1.0, 1.0,   0.0, 1.0,    // v7-v4-v3-v2 down
+  0.0, 0.0,   1.0, 0.0,   1.0, 1.0,   0.0, 1.0     // v4-v7-v6-v5 back
+]);
+
+  // prettier-ignore
+  // Indices of the vertices
+  var indices = new Uint8Array([
+    0, 1, 2,   0, 2, 3,    // front
+    4, 5, 6,   4, 6, 7,    // right
+    8, 9,10,   8,10,11,    // up
+   12,13,14,  12,14,15,    // left
+   16,17,18,  16,18,19,    // down
+   20,21,22,  20,22,23     // back
+   ]);
+
+  // Write vertex information to buffer object
+  if (!initArrayBuffer(gl, verticesPositions, 3, gl.FLOAT, "a_Position"))
+    return -1; // Vertex coordinates
+  if (!initArrayBuffer(gl, texCoords, 2, gl.FLOAT, "a_TexCoord")) return -1; // Texture coordinates
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  const index_buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  return indices.length;
+}
+
+function initArrayBuffer(gl, data, num, type, attribute) {
+  // Create a buffer object
+  var buffer = gl.createBuffer();
+  if (!buffer) {
+    console.log("Failed to create the buffer object");
+    return false;
+  }
+  // Write date into the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+  // Assign the buffer object to the attribute variable
+  var a_attribute = gl.getAttribLocation(gl.program, attribute);
+  if (a_attribute < 0) {
+    console.log("Failed to get the storage location of " + attribute);
+    return false;
+  }
+  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+  // Enable the assignment to a_attribute variable
+  gl.enableVertexAttribArray(a_attribute);
+
+  return true;
+}
+
+function initTextures(gl) {
+  var texture = gl.createTexture();
+  var u_Sampler = gl.getUniformLocation(gl.program, "u_Sampler");
+  var image = new Image();
+  image.onload = function () {
+    loadTexture(gl, texture, u_Sampler, image);
+  };
+  image.src = "../resources/sky.jpg";
+  return true;
+}
+
+function loadTexture(gl, texture, u_Sampler, image) {
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  gl.uniform1i(u_Sampler, 0);
+}
