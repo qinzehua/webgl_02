747bdd21cb5329510b9481a9ce463e398863690c
qin.zehua <775935087@qq.com>
2022-07-25 22:19:19 +0800

    双shader

--- chapter10/ProgramObject.js
+++ chapter10/ProgramObject.js
@@ -1,6 +1,11 @@
 var VSHADER_SOURCE = document.getElementById("vshader").innerHTML;
 var FSHADER_SOURCE = document.getElementById("fshader").innerHTML;
 
+var VSHADER_TEXTURE_SOURCE =
+  document.getElementById("vshader-texture").innerHTML;
+var FSHADER_TEXTURE_SOURCE =
+  document.getElementById("fshader-texture").innerHTML;
+
 var canvas = document.getElementById("webgl");
 
 function main() {
@@ -23,7 +28,32 @@ function main() {
     "u_NormalMatrix"
   );
 
+  const textureProgram = createProgram(
+    gl,
+    VSHADER_TEXTURE_SOURCE,
+    FSHADER_TEXTURE_SOURCE
+  );
+  textureProgram.a_Position = gl.getAttribLocation(
+    textureProgram,
+    "a_Position"
+  );
+  textureProgram.a_Normal = gl.getAttribLocation(textureProgram, "a_Normal");
+  textureProgram.a_TexCoord = gl.getAttribLocation(
+    textureProgram,
+    "a_TexCoord"
+  );
+  textureProgram.u_MvpMatrix = gl.getUniformLocation(
+    textureProgram,
+    "u_MvpMatrix"
+  );
+  textureProgram.u_NormalMatrix = gl.getUniformLocation(
+    textureProgram,
+    "u_NormalMatrix"
+  );
+  textureProgram.u_Sampler = gl.getUniformLocation(textureProgram, "u_Sampler");
+
   var cube = initVertexBuffers(gl);
+  var texture = initTextures(gl, textureProgram);
 
   gl.clearColor(0.0, 0.0, 0.0, 1);
   gl.enable(gl.DEPTH_TEST);
@@ -33,6 +63,7 @@ function main() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     drawSolidCube(gl, cube, solideProgram);
+    draw(gl, cube, textureProgram, texture);
     requestAnimationFrame(tick);
   };
   tick();
@@ -46,6 +77,21 @@ const modalMatrix = new Matrix4();
 var g_MvpMatrix = new Matrix4();
 var g_NormalMatrix = new Matrix4();
 
+function draw(gl, cube, textureProgram, texture) {
+  gl.useProgram(textureProgram);
+
+  initAttributeVariable(gl, textureProgram.a_Position, cube.positionBuffer);
+  initAttributeVariable(gl, textureProgram.a_TexCoord, cube.texCoordsBuffer);
+  initAttributeVariable(gl, textureProgram.a_Normal, cube.normalBuffer);
+
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube.index_buffer);
+
+  drawCube(gl, textureProgram, cube, 2);
+}
+
 function drawSolidCube(gl, cube, solideProgram) {
   gl.useProgram(solideProgram);
   initAttributeVariable(gl, solideProgram.a_Position, cube.positionBuffer);
@@ -53,20 +99,20 @@ function drawSolidCube(gl, cube, solideProgram) {
   initAttributeVariable(gl, solideProgram.a_Normal, cube.normalBuffer);
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube.index_buffer);
 
-  modalMatrix.setTranslate(-2, 0.0, 0.0);
+  drawCube(gl, solideProgram, cube, -2);
+}
+
+function drawCube(gl, program, cube, x) {
+  modalMatrix.setTranslate(x, 0.0, 0.0);
   modalMatrix.rotate(20.0, 1.0, 0.0, 0.0);
   modalMatrix.rotate(angle, 0.0, 1.0, 0.0);
 
   g_MvpMatrix.set(verticx4).multiply(modalMatrix);
-  gl.uniformMatrix4fv(solideProgram.u_MvpMatrix, false, g_MvpMatrix.elements);
+  gl.uniformMatrix4fv(program.u_MvpMatrix, false, g_MvpMatrix.elements);
 
   g_NormalMatrix.setInverseOf(modalMatrix);
   g_NormalMatrix.transpose();
-  gl.uniformMatrix4fv(
-    solideProgram.u_NormalMatrix,
-    false,
-    g_NormalMatrix.elements
-  );
+  gl.uniformMatrix4fv(program.u_NormalMatrix, false, g_NormalMatrix.elements);
   gl.drawElements(gl.TRIANGLES, cube.indicsLenght, gl.UNSIGNED_BYTE, 0);
 }
 
@@ -108,6 +154,16 @@ function initVertexBuffers(gl) {
 ]);
 
   // prettier-ignore
+  var texCoords = new Float32Array([   // Texture coordinates
+1.0, 1.0,   0.0, 1.0,   0.0, 0.0,   1.0, 0.0,    // v0-v1-v2-v3 front
+0.0, 1.0,   0.0, 0.0,   1.0, 0.0,   1.0, 1.0,    // v0-v3-v4-v5 right
+1.0, 0.0,   1.0, 1.0,   0.0, 1.0,   0.0, 0.0,    // v0-v5-v6-v1 up
+1.0, 1.0,   0.0, 1.0,   0.0, 0.0,   1.0, 0.0,    // v1-v6-v7-v2 left
+0.0, 0.0,   1.0, 0.0,   1.0, 1.0,   0.0, 1.0,    // v7-v4-v3-v2 down
+0.0, 0.0,   1.0, 0.0,   1.0, 1.0,   0.0, 1.0     // v4-v7-v6-v5 back
+]);
+
+  // prettier-ignore
   var indices = new Uint8Array([
     0, 1, 2,   0, 2, 3,    // front
     4, 5, 6,   4, 6, 7,    // right
@@ -121,6 +177,7 @@ function initVertexBuffers(gl) {
   o.positionBuffer = prepareArrayBuffer(gl, verticesPositions, 3, gl.FLOAT);
   o.colorBuffer = prepareArrayBuffer(gl, colors, 3, gl.FLOAT);
   o.normalBuffer = prepareArrayBuffer(gl, normals, 3, gl.FLOAT);
+  o.texCoordsBuffer = prepareArrayBuffer(gl, texCoords, 2, gl.FLOAT);
 
   o.index_buffer = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, o.index_buffer);
@@ -131,6 +188,29 @@ function initVertexBuffers(gl) {
 
   o.indicsLenght = indices.length;
   return o;
+}
+
+function initTextures(gl, program) {
+  var texture = gl.createTexture();
+
+  var image = new Image();
+
+  image.onload = function () {
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+    gl.useProgram(program);
+    gl.uniform1i(program.u_Sampler, 0);
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+  };
+
+  image.src = "../resources/orange.jpg";
+
+  return texture;
 }
 
 function prepareArrayBuffer(gl, data, num, type) {
